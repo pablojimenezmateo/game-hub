931ea20796758cfb22aaa39dbea7d80878914ffb
Pablo <pablo.jimenez@exheus.com>
2023-05-29 20:35:42 +0200

    feat: usePlatforms now uses react query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
-import axios from "axios";
 import apiClient from "../services/apiClient";
 import { FetchResponse } from "./useData";
 
@@ -12,8 +11,9 @@ export interface Genre {
 
 const useGenres = () => useQuery({
       queryKey: ["genres"],
-      queryFn: () => apiClient.get<FetchResponse<Genre>>("/genres")
-              .then((res) => res.data),
+      queryFn: () => apiClient
+                .get<FetchResponse<Genre>>("/genres")
+                .then((res) => res.data),
       staleTime: 24 * 60 * 60 * 1000, // 24h
       initialData: {count: genres.length, results: genres}
 })
