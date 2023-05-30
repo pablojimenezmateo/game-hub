003f299e261decc0d7e0f304de8823027dde1999
Pablo <pablo.jimenez@exheus.com>
2023-05-30 10:32:04 +0200

    feat: implement infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/apiClient";
 import { Platform } from "./usePlatforms";
@@ -15,20 +15,41 @@ export interface Game {
 
 const apiClient = new APIClient<Game>("/games");
 
-const useGames = (gameQuery: GameQuery) =>
-    useQuery<FetchResponse<Game>, Error>({
-    queryKey: ["games", gameQuery],
-    queryFn: () => apiClient
-              .getAll({
-                params: {
-                    genres: gameQuery.genre?.id,
-                    parent_platforms: gameQuery.platform?.id,
-                    ordering: gameQuery.sortOrder,
-                    search: gameQuery.searchText
-                }
-            }),
-    staleTime: 1 * 60 * 60 * 1000, // 1h
-})
+const useGames = (gameQuery: GameQuery) => {
+
+    return useInfiniteQuery<FetchResponse<Game>, Error>({
+        queryKey: ["games", gameQuery],
+        queryFn: ({pageParam = 1}) => apiClient
+                  .getAll({
+                    params: {
+                        genres: gameQuery.genre?.id,
+                        parent_platforms: gameQuery.platform?.id,
+                        ordering: gameQuery.sortOrder,
+                        search: gameQuery.searchText,
+                        page: pageParam
+                    }
+                }),
+        staleTime: 1 * 60 * 60 * 1000, // 1h
+        keepPreviousData: true,
+        getNextPageParam: (lastPage, allPages) => {
+          return lastPage.next? allPages.length + 1 : undefined;
+        }
+    })
+};
+
+    // useQuery<FetchResponse<Game>, Error>({
+    // queryKey: ["games", gameQuery],
+    // queryFn: () => apiClient
+    //           .getAll({
+    //             params: {
+    //                 genres: gameQuery.genre?.id,
+    //                 parent_platforms: gameQuery.platform?.id,
+    //                 ordering: gameQuery.sortOrder,
+    //                 search: gameQuery.searchText
+    //             }
+    //         }),
+    // staleTime: 1 * 60 * 60 * 1000, // 1h
+// })
 
 export default useGames;
 
