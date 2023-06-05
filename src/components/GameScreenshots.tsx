59cfa5d071c6e0b431f386f787de0924eb19dbbb
Pablo <pablo.jimenez@exheus.com>
2023-06-05 11:42:45 +0200

    feat: add game screenshots

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,23 @@
+import useScreenshots from "../hooks/useScreenshots";
+import { SimpleGrid, Image } from "@chakra-ui/react";
+
+interface Props {
+  gameId: number;
+}
+
+const GameScreenshots = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useScreenshots(gameId);
+
+  if (isLoading) return null;
+  if (error) throw error;
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+      {data?.results.map((image) => (
+        <Image key={image.id} src={image.image} />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameScreenshots;
